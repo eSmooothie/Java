6a60873313c29e28720006aeb84196236db955ad
Sherwin Sandoval <33995402+eSmooothie@users.noreply.github.com>
2021-10-01 22:13:43 +0800

    Add files via upload

--- Cryptography.java
+++ Cryptography.java
@@ -6,199 +6,356 @@
 package Exercise1; // folder name
 
 // imports
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Main
  * 
  */
 class Main {
-    public static void main(String args[]) {
-        // instantiate
-        Vigenere vigenereCipher = new Vigenere();
+    public static void main(String args[]) throws IOException {
+        // select two primes p and q, p != q
+        int p = 11;
+        int q = 13;
 
-        // agreed key
-        String key = "lemon";
+        RSA rsa = new RSA(p, q);
 
-        // encryption
-        String plainText = "attack at dawn";
-        vigenereCipher.encrypt(plainText, key);
-        System.out.println("Plain Message: " + vigenereCipher.getPlainMessage());
-        System.out.println("Key: " + vigenereCipher.getKey());
-        System.out.println("Encrypted Message: " + vigenereCipher.getEncryptedMessage());
+        // Choose e such that,
+        // 1 < e < phi of n
+        // coprime with n and phi of n
+        // int e = <something>
+        // rsa.setE(e); // manually set e
+        rsa.generateE(); // generate e
+
+        // choose d such that
+        // e * d mod phi = 1
+        // int d = <something>
+        // rsa.setD(d); // manually set d
+        rsa.computeD();
+
+        System.out.println("RSA Encryption -----------------");
+
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader rd = new BufferedReader(reader);
+        System.out.print("Enter message: ");
+
+        String plainMessage = rd.readLine();
+
+        System.out.println("\nPlain Message: " + plainMessage);
+        System.out.print("Encrypted Message: ");
+        rsa.encrypt(plainMessage); // C = M^e mod n
+
         System.out.println();
 
-        // decryption
-        String cipherText = "lxfopvefrnhr";
-        vigenereCipher.decrypt(cipherText, key);
-        System.out.println("Encrypted Message: " + vigenereCipher.getEncryptedMessage());
-        System.out.println("Key: " + vigenereCipher.getKey());
-        System.out.println("Plain Message: " + vigenereCipher.getPlainMessage());
+        System.out.print("Decrypted Message: ");
+        String cipherMessage = rsa.getEncryptedMessage().toString();
+        rsa.decrypt(cipherMessage); // M = C^d mod n
 
+        System.out.println("\n--------------------------------");
     }
 }
 
-/**
- * Vigenere cipher text
- * 
- * 
- */
-class Vigenere extends Cryptography {
-    /**
-     * Evaluate if the object is a string
-     * 
-     * @param obj
-     * @return true if string otherwise false
-     */
-    private boolean isString(Object obj) {
-        return obj instanceof String;
+class RSA extends Cryptography {
+
+    private HashMap<String, Object> keys = new HashMap<String, Object>();
+    private int n; // product of two prime number
+    private int phi;
+    private int e; // encryption key
+    private int d; // decryption key
+
+    RSA(int p, int q) {
+        if (p == q) {
+            System.err.println("Error p must not be equal to q.");
+            System.exit(0);
+        }
+        if (!isPrime(p) && !isPrime(q)) {
+            System.err.println("Error p and q must be prime numbers.");
+            System.exit(0);
+        }
+        // calculate n = p * q
+        this.n = p * q;
+        // calculate phi of n = (p - 1) * (q - 1)
+        this.phi = (p - 1) * (q - 1);
     }
 
     /**
-     * Remove white spaces in a string
+     * Evaluate if n is a prime number.
      * 
-     * Example input " ABC DEF "
-     * 
-     * @param plainMessage
-     * @return A no whitespace string, example output: "ABCDEF"
+     * @param n
+     * @return true if n is prime otherwise false
      */
-    private String removeWhitespace(String message) {
-        // remove space
-        return message.replace(" ", "");
+    public static boolean isPrime(int n) {
+
+        // Check if number is less than
+        // equal to 1
+        if (n <= 1)
+            return false;
+
+        // Check if number is 2
+        else if (n == 2)
+            return true;
+
+        // Check if n is a multiple of 2
+        else if (n % 2 == 0)
+            return false;
+
+        // If not, then just check the odds
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
     }
 
     /**
-     * Convert a string to decimal
+     * Generate a prime numbers between 2 to n
      * 
-     * @param string
-     * @return a list of integer from [0-25], where 0 = A ... 25 = Z
+     * @param n
+     * @return random prime numbers between 2 to n
      */
-    private int[] stringToDecimal(String str) {
-        str = str.toUpperCase();
+    public static Integer generatePrimeNumbers(int n) {
+        boolean prime[] = new boolean[n + 1];
+        Arrays.fill(prime, true);
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p]) {
+                for (int i = p * 2; i <= n; i += p) {
+                    prime[i] = false;
+                }
+            }
+        }
+        List<Integer> primeNumbers = new LinkedList<>();
+        for (int i = 2; i <= n; i++) {
+            if (prime[i]) {
+                primeNumbers.add(i);
+            }
+        }
+        // randomize
+        Random rand = new Random();
+        int index = rand.nextInt(primeNumbers.size());
+        return primeNumbers.get(index);
+    }
 
-        int n = str.length(); // string size
+    /**
+     * Get all coprimes of n
+     * 
+     * @param n
+     * @return coprimes of n
+     */
+    public static List<Integer> getCoprimes(int n) {
+        LinkedList<Integer> coprimes = new LinkedList<Integer>();
+        for (int i = 2; i < n; i++) {
+            boolean isCoprime = isRelativePrime(i, n);
+            if (isCoprime) {
+                coprimes.add(i);
+            }
+        }
+        return coprimes;
+    }
 
-        int[] dec = new int[n];
-
-        char[] characters = str.toCharArray();
-
-        for (int i = 0; i < characters.length; i++) {
-            // read each character in a string
-            // and get its corresponding integer value
-
-            char c = characters[i];
-
-            int d = Integer.valueOf(c) - 65; // 65 = A
-
-            // check for non alphabet character
-            if (d < 0 || d > 25) {
-                // terminate
-                System.err.println("Error: Non alphabet charcater");
-                System.exit(0);
+    /**
+     * Evaluate if integer a is ralative prime to integer b
+     * 
+     * @param a
+     * @param b
+     * @return true if relative prime otherwise false
+     */
+    public static boolean isRelativePrime(int a, int b) {
+        int gcd = 1;
+        int min = a;
+        int max = b;
+        while (max > min) {
+            int r = max % min;
+            if (r == 0) {
+                gcd = min;
+                break;
             } else {
+                max = min;
+                min = r;
+            }
+        }
+        if (gcd == 1) {
+            return true;
+        }
+        return false;
+    }
 
-                dec[i] = d;
+    /**
+     * compute the d(decryption number), using the formula ed mod n = 1
+     */
+    public void computeD() {
+        if (this.phi == 0 || this.e == 0) {
+            System.out.println("Error: phi and e not set.");
+            System.exit(0);
+        }
+        int d = -1;
+        int i = 1;
+        while (d <= this.e) {
+            int x = ((this.phi * i) + 1) % this.e;
+            if (x == 0) {
+                d = ((this.phi * i) + 1) / this.e;
+            }
 
+            i++;
+        }
+        this.d = d;
+        int[] privateKey = { this.d, this.n };
+        this.keys.put("privateKey", privateKey);
+        this.setKey(keys);
+    }
+
+    /**
+     * Generate e(encryption number) by checking the co primes of phi and n
+     */
+    public void generateE() {
+        if (this.phi == 0 && this.n == 0) {
+            System.err.println("Error phi and n is not set");
+            System.exit(0);
+        }
+
+        int e = 0; // encryption key
+        List<Integer> coprimesOfPhi = RSA.getCoprimes(this.phi); // get coprimes of phi
+        List<Integer> coprimesOfN = RSA.getCoprimes(this.n); // get coprimes of n
+        HashMap<Integer, Boolean> coPrimes = new HashMap<Integer, Boolean>();
+
+        // map coprimesOfPhi
+        for (int i = 0; i < coprimesOfPhi.size(); i++) {
+            coPrimes.put(coprimesOfPhi.get(i), true);
+        }
+        // search for matching coprimes of phi and n
+        for (int i = 0; i < coprimesOfN.size(); i++) {
+            int prime = coprimesOfN.get(i);
+            if (coPrimes.containsKey(prime)) {
+                e = prime;
+                break;
             }
         }
 
-        return dec;
+        this.e = e;
+        int[] publicKey = { this.e, this.n };
+        this.keys.put("publicKey", publicKey);
+        this.setKey(keys);
     }
 
     /**
-     * Convert list of decimal into a string
+     * Manually set e(encryption number)
      * 
-     * Ex. input: [0,1,2]
-     * 
-     * @param decimal[]
-     * @return converted string, ex. output: ABC
+     * @param value
      */
-    private String decimalToString(int[] decimal) {
-        String str = "";
-        for (int i = 0; i < decimal.length; i++) {
-            int d = decimal[i] + 65;
-            if (d < 0 || d > 25) {
-                // terminate
-                System.err.println("Error: Invalid character.");
-                System.exit(0);
-            } else {
-                String c = Character.toString((char) d);
-                str = str + c;
+    public void setE(int value) {
+        if (isRelativePrime(value, this.phi) && isRelativePrime(value, this.n) && (value > 1 && value < phi)) {
+            this.e = value;
+            int[] publicKey = { this.e, this.n }; // set public key
+            this.keys.put("publicKey", publicKey);
+            this.setKey(keys);
+        } else {
+            System.err.println("Error: value must be a relative prime of phi and n or 1 < value < phi");
+            System.exit(0);
+        }
+    }
+
+    public void setD(int value) {
+        if ((this.e * value) % this.phi == 1 && value != this.e) {
+            this.d = value;
+            int[] privateKey = { this.d, this.n };
+            this.keys.put("privateKey", privateKey);
+            this.setKey(keys);
+        } else {
+            System.err.println("Error: ed mod phi = 1 and value != e");
+        }
+    }
+
+    @Override
+    void encrypt(String plainText) {
+
+        if (this.n == 0 && this.e == 0) {
+            System.out.println("Public key is not set");
+        }
+
+        String message = plainText;
+
+        this.setPlainMessage(message);
+
+        // get byte data of each characters
+        byte[] rawData = message.getBytes();
+
+        BigInteger[] encrypted = new BigInteger[rawData.length];
+        BigInteger e = new BigInteger(String.valueOf(this.e));
+        BigInteger n = new BigInteger(String.valueOf(this.n));
+
+        // C = M^e mod n, M < n
+        for (int i = 0; i < rawData.length; i++) {
+            BigInteger m = new BigInteger(String.valueOf(rawData[i]));
+            BigInteger C = m.modPow(e, n);
+            encrypted[i] = C;
+        }
+        String cipherText = "";
+        // transform to byte data
+        for (int i = 0; i < encrypted.length; i++) {
+            BigInteger data = encrypted[i];
+            byte transformData = (byte) (data.intValue());
+            char correspondingChar = (char) transformData;
+            System.out.print(correspondingChar);
+            cipherText += correspondingChar;
+        }
+
+        this.setEncryptedMessage(cipherText);
+    }
+
+    @Override
+    void decrypt(String cipherText) {
+
+        if (this.d == 0 && this.n == 0) {
+            System.out.println("Private key is not set");
+        }
+
+        String message = cipherText;
+
+        // get byte data of each characters
+        byte[] rawData = message.getBytes();
+
+        LinkedList<Integer> tData = new LinkedList<Integer>();
+        // inspect raw data
+        // this block examine the actual encrypted value of character
+        for (int i = 0; i < rawData.length; i++) {
+            byte x = rawData[i];
+            if (x != -17 && x != -66) {
+                if (x < 0) {
+                    int y = 129 + x; // 129 - x
+                    int actualData = 127 + y; // 127 + y
+                    tData.add(actualData);
+                } else if (x >= 0) {
+                    tData.add((int) x);
+                }
             }
-
         }
-        return str;
-    }
+        BigInteger[] decrypt = new BigInteger[tData.size()];
+        BigInteger d = new BigInteger(String.valueOf(this.d));
+        BigInteger n = new BigInteger(String.valueOf(this.n));
 
-    @Override
-    void encrypt(Object obj, Object key) {
-        // check obj or key is an instance of a String
-        if (!isString(obj) || !isString(key)) {
-            // terminate
-            System.err.println("Error: Invalid data type.");
-            System.exit(0);
-        }
-        // remove white spaces
-        String plainMessage = removeWhitespace(obj.toString());
-        String strKey = removeWhitespace(key.toString());
-
-        this.setPlainMessage(plainMessage.toUpperCase());
-        this.setKey(strKey.toUpperCase());
-
-        // convert each char into decimal
-        int[] decPlaintext = stringToDecimal(plainMessage);
-        int[] decKey = stringToDecimal(strKey);
-
-        // Encryption
-        int[] encryptedDec = new int[decPlaintext.length];
-
-        // start encrypting
-
-        // Eq. C[i] = (M[i] + K[i mod K.len]) mod 26
-        for (int i = 0; i < encryptedDec.length; i++) {
-            int M = decPlaintext[i]; // plain message
-            int K = decKey[(i % decKey.length)]; // repeated key
-            int C = (M + K) % 26; // cipher message
-            encryptedDec[i] = C;
+        // M = C^d mod n, C < n
+        for (int i = 0; i < tData.size(); i++) {
+            BigInteger c = new BigInteger(String.valueOf(tData.get(i)));
+            BigInteger M = c.modPow(d, n);
+            decrypt[i] = M;
         }
 
-        String encryptedMessage = decimalToString(encryptedDec);
-
-        this.setEncryptedMessage(encryptedMessage);
-    }
-
-    @Override
-    void decrypt(Object obj, Object key) {
-        // check obj or key is an instance of a String
-        if (!isString(obj) || !isString(key)) {
-            // terminate
-            System.err.println("Error: Invalid data type.");
-            System.exit(0);
+        String plainMessage = "";
+        // transform to byte data
+        for (int i = 0; i < decrypt.length; i++) {
+            BigInteger data = decrypt[i];
+            byte transformData = (byte) (data.intValue());
+            char correspondingChar = (char) transformData;
+            System.out.print(correspondingChar);
+            plainMessage += correspondingChar;
         }
-        // remove white spaces
-        String encryptedMessage = removeWhitespace(obj.toString());
-        String strKey = removeWhitespace(key.toString());
-
-        this.setEncryptedMessage(encryptedMessage.toUpperCase());
-        this.setKey(strKey.toUpperCase());
-
-        // convert each char into decimal
-        int[] decCipher = stringToDecimal(encryptedMessage);
-        int[] decKey = stringToDecimal(strKey);
-
-        // Decryption
-        int[] decryptedDec = new int[decCipher.length];
-
-        // start decrypting
-
-        // Eq. M[i] = (C[i] - K[i mod K.len] + 26) mod 26
-        for (int i = 0; i < decryptedDec.length; i++) {
-            int C = decCipher[i]; // cipher message
-            int K = decKey[(i % decKey.length)]; // repeated key
-            int M = (C - K + 26) % 26; // plain message
-            decryptedDec[i] = M;
-        }
-
-        String plainMessage = decimalToString(decryptedDec);
 
         this.setPlainMessage(plainMessage);
     }
@@ -245,17 +402,15 @@ abstract class Cryptography {
     /**
      * Encryption
      * 
-     * @param obj // to encrypt
-     * @param key
+     * @param plainText // to encrypt
      */
-    abstract void encrypt(Object obj, Object key);
+    abstract void encrypt(String plainText);
 
     /**
      * 
      * Decryption
      * 
-     * @param obj // to decrypt
-     * @param key
+     * @param cipherText // to decrypt
      */
-    abstract void decrypt(Object obj, Object key);
+    abstract void decrypt(String cipherText);
 }
